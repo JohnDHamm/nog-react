18f96eb2718bf75f9cec933df6fed39e6c42c606
John Hamm <johndhamm1@gmail.com>
2020-04-22 12:48:03 -0500

    add styled-components to function-library

---
+++ packages/function-library/src/TestSC/TestSC.styles.ts
@@ -0,0 +1,11 @@
+import styled from "styled-components"
+
+export const Container = styled.div`
+  border: 1px solid green;
+  background-color: grey;
+`
+
+export const Text = styled.p`
+  color: white;
+  font-size: 1.5rem;
+`
--- packages/function-library/src/index.ts
+++ packages/function-library/src/index.ts
@@ -1,3 +1,7 @@
+import TestSC from "./TestSC/TestSC"
+
 export const testFunction = (name: string) => {
   return `Hello, ${name}.`
 }
+
+export { TestSC }
