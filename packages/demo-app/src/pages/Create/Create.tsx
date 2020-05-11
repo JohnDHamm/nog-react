778627343fe7376471edd08e3e5b99da1df7d010
John Hamm <johndhamm1@gmail.com>
2020-05-11 14:51:10 -0500

    feat(demo-app): add routing + 3 pages

---
+++ packages/demo-app/src/pages/Create/Create.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Create: React.FC = () => {
+  return (
+    <div>
+      <h3>Create page</h3>
+    </div>
+  );
+};
+
+export default Create;
---
+++ packages/demo-app/src/pages/Play/Play.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Play: React.FC = () => {
+  return (
+    <div>
+      <h3>Play page</h3>
+    </div>
+  );
+};
+
+export default Play;
