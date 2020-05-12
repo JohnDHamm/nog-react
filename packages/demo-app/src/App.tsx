7a553182a0c49b63fabf37dc74aa99713b60d37c
John Hamm <johndhamm1@gmail.com>
2020-05-12 02:23:48 -0500

    fix(demo-app): remove NavBar

--- packages/demo-app/src/App.tsx
+++ packages/demo-app/src/App.tsx
@@ -1,12 +1,10 @@
 import React from 'react';
 import { BrowserRouter as Router, Link, Route, Switch } from 'react-router-dom';
 import { Create, Home, Play } from './pages';
-import { NavBar } from 'ui-library';
 
 function App() {
   return (
     <Router>
-      <NavBar />
       <Link to="/">
         <p>Nog Demo App</p>
       </Link>
