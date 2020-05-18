d96ae3969083d088095cad5fda7e7bbe41613b17
John Hamm <johndhamm1@gmail.com>
2020-05-17 22:29:03 -0500

    feat(demo-app): add SnowflakeInstances to CreatePage

--- packages/demo-app/src/pages/CreatePage/CreatePage.styles.ts
+++ packages/demo-app/src/pages/CreatePage/CreatePage.styles.ts
@@ -16,4 +16,17 @@ export const MainContent = styled.div`
   width: 100%;
   height: 100vh;
   background: #1a1a1a;
+  text-align: center;
+`;
+
+export const InstanceBlock = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  padding: 1rem 0;
+`;
+
+export const EmptyInstance = styled.div<{ size: number }>`
+  width: ${props => props.size + 'px'};
+  height: ${props => props.size + 'px'};
 `;
