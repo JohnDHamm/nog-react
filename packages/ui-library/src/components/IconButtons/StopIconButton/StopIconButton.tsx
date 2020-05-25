800278c3483eac8a475206f16bdc21327bde2577
John Hamm <johndhamm1@gmail.com>
2020-05-25 16:04:22 -0500

    refactor(StopIconButton): revise svg

--- packages/ui-library/src/components/IconButtons/StopIconButton/StopIconButton.tsx
+++ packages/ui-library/src/components/IconButtons/StopIconButton/StopIconButton.tsx
@@ -27,7 +27,7 @@ const StopIconButton: React.FC<IconButtonProps> = ({
           d="M180,262.28A82.28,82.28,0,1,1,262.28,180,82.37,82.37,0,0,1,180,262.28Zm0-150.56A68.28,68.28,0,1,0,248.28,180,68.36,68.36,0,0,0,180,111.72Z"
           transform="translate(-97.72 -97.72)"
         />
-        <rect x="46.06" y="45.56" width="73.46" height="73.46" />
+        <rect x="52.68" y="52.18" width="60.21" height="60.21" />
       </g>
     </BaseIconButton>
   );
