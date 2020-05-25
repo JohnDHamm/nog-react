1f2082b2b996c708c0a6d3b8ebffd98c9efac658
John Hamm <johndhamm1@gmail.com>
2020-05-25 18:22:31 -0500

    chore(demo-app): remove NavBar local component

--- packages/demo-app/src/App.tsx
+++ packages/demo-app/src/App.tsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import { CreatePage, Home, PlayPage } from './pages';
-import NavBar from './components/NavBar/NavBar';
+import { NavBar } from 'ui-library';
+
+const GuestAvatar = require('./assets/guest_avatar.png');
 
 function App() {
   return (
     <Router>
-      <NavBar />
+      <NavBar avatarSrc={GuestAvatar} userName="Guest" />
       <Switch>
         <Route path="/create" component={CreatePage} />
         <Route path="/play" component={PlayPage} />
