dd6e8156466473342b589335829c796156efb8e2
John Hamm <johndhamm1@gmail.com>
2021-03-05 13:36:45 -0600

    feat(ui-library): update FrostyMockSong

--- packages/ui-library/src/mocks/songs/FrostyMockSong.ts
+++ packages/ui-library/src/mocks/songs/FrostyMockSong.ts
@@ -101,25 +101,25 @@ export const FrostyMockSong: Song = {
         {
           timePosition: 1,
           length: 'quarter',
-          scalePosition: 5,
+          scalePosition: 9,
           type: 'note',
         },
         {
           timePosition: 5,
           length: 'quarter',
-          scalePosition: 5,
+          scalePosition: 8,
           type: 'note',
         },
         {
           timePosition: 9,
           length: 'quarter',
-          scalePosition: 5,
+          scalePosition: 7,
           type: 'note',
         },
         {
           timePosition: 13,
           length: 'quarter',
-          scalePosition: 5,
+          scalePosition: 6,
           type: 'note',
         },
       ],
@@ -147,6 +147,231 @@ export const FrostyMockSong: Song = {
         {
           timePosition: 13,
           text: 'py',
+        },
+      ],
+    },
+    {
+      barNumber: 4,
+      notes: [
+        {
+          timePosition: 1,
+          length: 'half-dotted',
+          scalePosition: 5,
+          type: 'note',
+        },
+        {
+          timePosition: 13,
+          length: 'eighth',
+          scalePosition: 7,
+          type: 'note',
+        },
+        {
+          timePosition: 15,
+          length: 'eighth',
+          scalePosition: 8,
+          type: 'note',
+        },
+      ],
+      lyrics: [
+        {
+          timePosition: 1,
+          text: 'soul,',
+        },
+        {
+          timePosition: 13,
+          text: 'with',
+        },
+        {
+          timePosition: 15,
+          text: 'a',
+        },
+      ],
+    },
+    {
+      barNumber: 5,
+      notes: [
+        {
+          timePosition: 1,
+          length: 'quarter',
+          scalePosition: 9,
+          type: 'note',
+        },
+        {
+          timePosition: 5,
+          length: 'quarter',
+          scalePosition: 8,
+          type: 'note',
+        },
+        {
+          timePosition: 9,
+          length: 'quarter',
+          scalePosition: 7,
+          type: 'note',
+        },
+        {
+          timePosition: 13,
+          length: 'eighth',
+          scalePosition: 6,
+          type: 'note',
+        },
+        {
+          timePosition: 15,
+          length: 'eighth',
+          scalePosition: 6,
+          type: 'note',
+        },
+      ],
+      lyrics: [
+        {
+          timePosition: 1,
+          text: 'corn',
+        },
+        {
+          timePosition: 5,
+          text: 'cob',
+        },
+        {
+          timePosition: 9,
+          text: 'pipe',
+        },
+        {
+          timePosition: 13,
+          text: 'and',
+        },
+        {
+          timePosition: 15,
+          text: 'a',
+        },
+      ],
+    },
+    {
+      barNumber: 6,
+      notes: [
+        {
+          timePosition: 1,
+          length: 'quarter',
+          scalePosition: 5,
+          type: 'note',
+        },
+        {
+          timePosition: 5,
+          length: 'quarter',
+          scalePosition: 8,
+          type: 'note',
+        },
+        {
+          timePosition: 9,
+          length: 'quarter',
+          scalePosition: 3,
+          type: 'note',
+        },
+        {
+          timePosition: 13,
+          length: 'eighth',
+          scalePosition: 5,
+          type: 'note',
+        },
+        {
+          timePosition: 15,
+          length: 'eighth',
+          scalePosition: 6,
+          type: 'note',
+        },
+      ],
+      lyrics: [
+        {
+          timePosition: 1,
+          text: 'but',
+        },
+        {
+          timePosition: 3,
+          text: '-',
+        },
+        {
+          timePosition: 5,
+          text: 'ton',
+        },
+        {
+          timePosition: 9,
+          text: 'nose',
+        },
+        {
+          timePosition: 13,
+          text: 'and',
+        },
+        {
+          timePosition: 15,
+          text: 'two',
+        },
+      ],
+    },
+    {
+      barNumber: 7,
+      notes: [
+        {
+          timePosition: 1,
+          length: 'quarter',
+          scalePosition: 5,
+          type: 'note',
+        },
+        {
+          timePosition: 5,
+          length: 'quarter',
+          scalePosition: 4,
+          type: 'note',
+        },
+        {
+          timePosition: 9,
+          length: 'quarter',
+          scalePosition: 3,
+          type: 'note',
+        },
+        {
+          timePosition: 13,
+          length: 'quarter',
+          scalePosition: 4,
+          type: 'note',
+        },
+      ],
+      lyrics: [
+        {
+          timePosition: 1,
+          text: 'eyes',
+        },
+        {
+          timePosition: 5,
+          text: 'made',
+        },
+        {
+          timePosition: 9,
+          text: 'out',
+        },
+        {
+          timePosition: 13,
+          text: 'of',
+        },
+      ],
+    },
+    {
+      barNumber: 8,
+      notes: [
+        {
+          timePosition: 1,
+          length: 'half-dotted',
+          scalePosition: 5,
+          type: 'note',
+        },
+        {
+          timePosition: 13,
+          length: 'quarter',
+          scalePosition: 7,
+          type: 'rest',
+        },
+      ],
+      lyrics: [
+        {
+          timePosition: 1,
+          text: 'coal',
         },
       ],
     },
