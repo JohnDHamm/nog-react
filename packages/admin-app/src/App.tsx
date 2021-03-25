0db8881d2d479cc4bcf569ad702b7451f3a81912
John Hamm <johndhamm1@gmail.com>
2021-03-25 16:44:10 -0500

    refactor(app, admin-app): remove test components and functions

--- packages/admin-app/src/App.tsx
+++ packages/admin-app/src/App.tsx
@@ -1,17 +1,11 @@
 import React from 'react';
 import './App.css';
-import { sayHello } from "function-library";
-import { TestComponent } from "ui-library"
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <p>
-          Nog Admin App
-        </p>
-        <p>{sayHello("your highness")} What can I do for you today?</p>
-        <TestComponent text="admin test" />
+        <p>Nog Admin App</p>
       </header>
     </div>
   );
