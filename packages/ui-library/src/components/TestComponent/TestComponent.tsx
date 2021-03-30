76b2d44e0c47267b2aaeb59b4e96bb5ae44f697d
John Hamm <johndhamm1@gmail.com>
2021-03-30 15:52:02 -0500

    feat(types): add global type config

---
+++ packages/ui-library/src/components/TestComponent/TestComponent.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+// TODO: use this to test Storybook working with global types
+// const testType: TestNogType = 'snowflake30';
+
+const TestComponent: React.FC = () => {
+  return <div>testType</div>;
+};
+
+export default TestComponent;
---
+++ types/Test.d.ts
@@ -0,0 +1,1 @@
+type TestNogType = 'snowflake30' | 'tannenbaum24';
